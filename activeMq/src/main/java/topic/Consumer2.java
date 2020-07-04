ff98a2da343e734dbcd48b55bd212308b0d03f60
gao@163.com <gao@163.com>
2020-07-05 00:42:18 +0800

    21

--- activeMq/src/main/java/topic/Consumer2.java
+++ activeMq/src/main/java/topic/Consumer2.java
@@ -1,4 +1,4 @@
-2222222222222
+2222222222222212
 public class Consumer2 {
     private static String  MQURL="tcp://localhost:61616";
     public static void main(String[] args) throws JMSException {
